78b943e32cc8a4d77b664c218e119b05aa14af29
YUVA TEJA REDDY JONNALA <yjwuk@umsystem.edum>
2025-12-04 14:32:04 -0600

    poll check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            YUUBAYABISBCBISAC YUVA
+            kirtidvidmv 
         </p>
         <a
           className="App-link"
