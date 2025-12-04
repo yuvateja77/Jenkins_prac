edba4941567ca1e760497ac415ec9acc098aba30
YUVA TEJA REDDY JONNALA <yjwuk@umsystem.edum>
2025-12-04 12:57:09 -0600

    CHECK

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Deploy check after pushing to main branch
+            YUUBAYABISBCBISAC YUVA
         </p>
         <a
           className="App-link"
